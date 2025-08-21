c4b533b7a187fc001cf112a4f80e06dd2edd1920
rainool <203097192+rainool@users.noreply.github.com>
2025-08-21 11:53:49 +1000

    Add files via upload

---
+++ MyTime.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTime
+{
+    internal class MyTime
+    {
+        //instance variables
+        private int _hour, _minute, _second;
+
+        //default constructor
+        public MyTime()
+        {
+            _hour = 0;
+            _minute = 0;
+            _second = 0;
+        }
+
+        //paraneterized constructor with validation
+        public MyTime(int hour, int minute, int second)
+        {
+            if (IsValidTime(hour, minute, second))
+            {
+                _hour = hour;
+                _minute = minute;
+                _second = second;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid hour, minute, or second.");
+            }
+        }
+
+        //validation helper methods
+        private bool IsValidTime(int hour, int minute, int second)
+        {
+            return hour >= 0 && hour <= 23 &&
+                minute >= 0 && minute <= 59 &&
+                second >= 0 && second <= 59;
+        }
+
+        private bool IsValidHour(int hour) => hour >= 0 && hour <= 23;
+        private bool IsValidMinute(int minute) => minute >= 0 && minute <= 59;
+        private bool IsValidSecond(int second) => second >= 0 && second <= 59;
+
+
+        //methods
+        public void SetTime(int hour, int minute, int second)
+        {
+            if (IsValidTime(hour, minute, second))
+            {
+                _hour = hour;
+                _minute = minute;
+                _second = second;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid hour, minute, or second.");
+            }
+        }
+
+        public void SetHour(int hour)
+        {
+            if (IsValidHour(hour))
+            {
+                _hour = hour;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid hour.");
+            }
+        }
+
+        public void SetMinute(int minute)
+        {
+            if (IsValidMinute(minute))
+            {
+                _minute = minute;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid minute.");
+            }
+        }
+
+        public void SetSecond(int second)
+        {
+            if (IsValidSecond(second))
+            {
+                _second = second;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid second.");
+            }
+        }
+
+        public int GetHour()
+        {
+            return _hour;
+        }
+
+        public int GetMinute()
+        {
+            return _minute;
+        }
+
+        public int GetSecond()
+        {
+            return _second;
+        }
+
+        public override string ToString()
+        {
+            return $"{_hour:D2}:{_minute:D2}:{_second:D2}";
+        }
+
+        public MyTime NextHour()
+        {
+            ++_hour;
+            if(_hour > 23)
+            {
+                _hour = 0;
+            }
+            return this;
+        }
+
+        public MyTime NextMinute()
+        {
+            ++_minute;
+            if(_minute > 59)
+            {
+                _minute = 0;      
+                NextHour();
+            }
+            return this;
+        }
+
+        public MyTime NextSecond()
+        {
+            ++_second;
+            if(_second > 59)
+            {
+                _second = 0;
+                NextMinute();
+            }
+            return this;
+        }
+
+        public MyTime PreviousHour()
+        {
+            --_hour;
+            if(_hour < 0)
+            {
+                _hour = 23;
+            }
+            return this;
+        }
+
+        public MyTime PreviousMinute()
+        {
+            --_minute;
+            if(_minute < 0)
+            {
+                _minute = 59;
+                PreviousHour();
+            }
+            return this;
+        }
+
+        public MyTime PreviousSecond()
+        {
+            --_second;
+            if(_second < 0)
+            {
+                _second = 59;
+                PreviousMinute();
+            }
+            return this;
+        }
+    }
+}
---
+++ TestMyTime.cs
@@ -0,0 +1,59 @@
+﻿namespace MyTime
+{
+    internal class TestMyTime
+    {
+        static void Main(string[] args)
+        {
+            //Default time
+            MyTime t1 = new MyTime();
+            Console.WriteLine("Default: " + t1.ToString());
+
+            //SetTime
+            t1.SetTime(12, 30, 45);
+            Console.WriteLine("SetTime: " + t1.ToString());
+
+            //SetHour, SetMinute, SetSecond
+            t1.SetHour(5);
+            t1.SetMinute(15);
+            t1.SetSecond(20);
+            Console.WriteLine("SetHour/Minute/Second: " + t1.ToString());
+
+            //GetHour, GetMinute, GetSecond
+            Console.WriteLine($"Hour: {t1.GetHour()}, Minute: {t1.GetMinute()}, Second: {t1.GetSecond()}");
+
+            //NextMinute
+            t1.SetTime(10, 59, 59);
+            t1.NextMinute();
+            Console.WriteLine("NextMinute: " + t1.ToString());
+
+            //PreviousMinute
+            t1.PreviousMinute();
+            Console.WriteLine("PreviousMinute: " + t1.ToString());
+
+            //NextHour
+            t1.SetTime(23, 59, 59);
+            t1.NextHour();
+            Console.WriteLine("NextHour: " + t1.ToString());
+
+            //PreviousHour
+            t1.PreviousHour();
+            Console.WriteLine("PreviousHour: " + t1.ToString());
+
+
+
+            //parameterized constructor
+            Console.WriteLine("---------------------------");
+            MyTime t2 = new MyTime(23, 59, 59);
+            Console.WriteLine("Parameterized: " + t2.ToString());
+
+            //NextSecond
+            t2.NextSecond();
+            Console.WriteLine("NextSecond: " + t2.ToString());
+
+            //PreviousSecond
+            t2.PreviousSecond();
+            Console.WriteLine("PreviousSecond: " + t2.ToString());
+
+        }
+    }
+}
